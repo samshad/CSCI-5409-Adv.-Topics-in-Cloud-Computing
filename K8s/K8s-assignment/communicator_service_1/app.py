7c57459f8aaf11813b788005062c7ec69de69ab3
Samshad <samshad.ru@gmail.com>
2024-07-08 21:59:29 -0300

    init

---
+++ K8s/K8s-assignment/communicator_service_1/app.py
@@ -0,0 +1,87 @@
+import os
+import requests
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+FOLDER_NAME = "/MdSamshad_PV_dir"
+
+
+@app.route("/calculate", methods=["POST"])
+def calculate():
+    """
+    Endpoint to calculate based on the provided file path.
+
+    Expects a JSON payload with a 'file' key containing the path to the file.
+
+    Returns:
+        dict: Response containing the status of the file and any error message.
+    """
+    try:
+        if request.json.get("file") is None:
+            return {
+                "file": None,
+                "error": "Invalid JSON input."
+            }
+    except KeyError:
+        return {
+            "file": None,
+            "error": "Invalid JSON input."
+        }
+
+    file_path = FOLDER_NAME + "/" + request.json["file"]
+    if not os.path.isfile(file_path):
+        return {
+            "file": file_path,
+            "error": "File not found."
+        }
+
+    sum_url = ""
+    response = requests.post(url=sum_url, json=request.json, headers={'Content-Type': 'application/json'})
+    return response.json()
+
+
+@app.route("/store-file", methods=["POST"])
+def store_file():
+    """
+    Endpoint to store the content to a file.
+
+    Expects a JSON payload with 'file' and 'data' keys.
+    The 'file' key should contain the path to the file.
+    The 'data' key should contain the content to be written to the file.
+
+    Returns:
+        dict: Response containing the status of the file storage and any error message.
+    """
+    try:
+        if request.json.get("file") is None:
+            return {
+                "file": None,
+                "error": "Invalid JSON input."
+            }
+    except KeyError:
+        return {
+            "file": None,
+            "error": "Invalid JSON input."
+        }
+
+    file_path = FOLDER_NAME + "/" + request.json["file"]
+    try:
+        with open(file_path, "w+") as csvfile:
+            csvfile.write(request.json["data"].replace(" ", ""))
+    except Exception as e:
+        return {
+            "file": None,
+            "error": "Error while storing the file to the storage."
+        }
+
+    return {
+        "file": file_path,
+        "message": "Success."
+    }
+
+
+if __name__ == "__main__":
+    app.json.sort_keys = False
+    app.run(host="0.0.0.0", port=6000, debug=True)
---
+++ K8s/K8s-assignment/file_processor_service_2/app.py
@@ -0,0 +1,44 @@
+import csv
+from flask import Flask, request
+
+app = Flask(__name__)
+
+FOLDER_NAME = "/MdSamshad_PV_dir"
+
+
+@app.route("/sum", methods=["POST"])
+def sum_products():
+    """
+    Endpoint to calculate the sum of products for a specified product from a CSV file.
+
+    Expects a JSON payload with 'file' and 'product' keys.
+    The 'file' key should contain the path to the CSV file.
+    The 'product' key should contain the product name to sum values for.
+
+    Returns:
+        dict: Response containing the file path and the calculated sum, or an error message.
+    """
+    sum_total = 0
+    file_path = FOLDER_NAME + "/" + request.json["file"]
+    product_name = request.json["product"]
+
+    with open(file_path) as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        for row in csv_reader:
+            if len(row) != 2:
+                return {
+                    "file": file_path,
+                    "error": "Input file not in valid CSV format."
+                }
+            if row[0] == product_name:
+                sum_total += int(row[1])
+
+    return {
+            "file": request.json["file"],
+            "sum": sum
+        }
+
+
+if __name__ == "__main__":
+    app.json.sort_keys = False
+    app.run(host="0.0.0.0", port=7000)
